a349cb6fe54931997df6bc7afaa431f3a117a010
shravankumar4 <shravan.kumar4@hp.com>
2014-12-04 14:43:14 +0530

    added first test

---
+++ src/test/java/BuildExampleTest.java
@@ -0,0 +1,13 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by kushrava on 12/4/2014.
+ */
+public class BuildExampleTest {
+
+    @Test
+    public void test(){
+        Assert.assertEquals(1,1);
+    }
+}
